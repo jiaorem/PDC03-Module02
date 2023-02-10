855e2319c1510218e18674b354844817db2d2069
Micoh Yabut <yabut.micohjomarie@auf.edu.ph>
2023-02-10 13:09:35 +0800

    First Commit

---
+++ PDC03_MODULE02/PDC03_MODULE02/Activity2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity2 : ContentPage
+    {
+        public Activity2()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged (object sender, ValueChangedEventArgs args)
+        {
+            valueLabel.Text= args.NewValue.ToString("F3");
+        }
+    }
+}
